0f5fb9279a5537b3edb6129d63c9190d6d240769
silent-rain <silent_rains@1163.com>
2022-07-16 14:20:29 +0800

    添加日志功能

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use dotenv::dotenv;
+use log;
+use log4rs;
 use rocket;
 
 use app_rocket::server;
@@ -7,6 +9,11 @@ use app_rocket::server;
 async fn main() -> Result<(), rocket::Error> {
     // 设置环境变量
     dotenv().ok();
+
+    // 初始化日志配置
+    if let Err(err) = log4rs::init_file("./log4rs.yaml", Default::default()) {
+        log::warn!("log init config error: {}", err);
+    }
 
     // 启动服务
     let _ = server().launch().await?;
