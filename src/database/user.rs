e52db9061cdc4d495fa76c608158b54005452d79
silent-rain <silent_rains@1163.com>
2022-07-17 16:25:09 +0800

    删除无用依赖

--- src/database/user.rs
+++ src/database/user.rs
@@ -1,15 +1,9 @@
-use base64;
-use chrono::{Duration, NaiveDateTime, Utc};
-
 use diesel::prelude::ExpressionMethods;
 use diesel::query_dsl::{QueryDsl, RunQueryDsl};
 use diesel::MysqlConnection;
 use diesel::QueryResult;
 
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
 use crate::models::auth;
 use crate::models::user::{Login, RegisterUser, User};
@@ -126,62 +120,8 @@ impl User {
     }
 }
 
-// impl User {
-//     /// 验证 candidate_password 是否与存储的密码匹配。
-//     pub fn verify_password(&self, candidate_password: &str) -> bool {
-//         let candidate_hash = argon2i_simple(candidate_password, SECRET).to_vec();
-//         self.password_hash == candidate_hash
-//     }
-
-//     /// 生成身份验证令牌并将其保存到 current_auth_token 列。
-//     pub fn generate_auth_token(&mut self, conn: &MysqlConnection) -> Result<String, DieselError> {
-//         let rng = thread_rng();
-//         let new_auth_token = rng.sample_iter(&Alphanumeric).take(32).collect::<String>();
-//         self.current_auth_token = Some(new_auth_token.clone());
-//         self.last_action = Some(Utc::now().naive_utc());
-//         self.save_changes::<User>(conn)?;
-//         Ok(new_auth_token)
-//     }
-
-//     /// 返回用户是否具有有效的身份验证令牌。
-//     pub fn has_valid_auth_token(&self, auth_token_timeout: Duration) -> bool {
-//         let latest_valid_date = Utc::now() - auth_token_timeout;
-//         if let Some(last_action) = self.last_action {
-//             if self.current_auth_token.is_some() {
-//                 last_action > latest_valid_date.naive_utc()
-//             } else {
-//                 false
-//             }
-//         } else {
-//             false
-//         }
-//     }
-
-//     /// Get a `User` from a login token.
-//     ///
-//     /// A login token has this format:
-//     ///     <user uuid>:<auth token>
-//     pub fn get_user_from_login_token(token: &str, db: &MysqlConnection) -> Option<User> {
-//         let v: Vec<&str> = token.split(':').collect();
-//         let user_id = Uuid::parse_str(v.get(0).unwrap_or(&"")).unwrap_or_default();
-//         let auth_token = v.get(1).unwrap_or(&"").to_string();
-
-//         let user = user::table.find(user_id).first::<User>(&*db).optional();
-//         if let Ok(Some(u)) = user {
-//             if let Some(token) = u.current_auth_token.clone() {
-//                 if verify_slices_are_equal(token.as_bytes(), auth_token.as_bytes()).is_ok() {
-//                     return Some(u);
-//                 }
-//             }
-//         }
-//         None
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn test_vec_u8_to_string() {
         let s: String = [10u8, 20u8, 30u8]
