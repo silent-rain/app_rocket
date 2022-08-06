509ab5c230376371ab3677748493c48c7713c511
silent-rain <silent_rains@1163.com>
2022-08-07 00:14:18 +0800

    业务错误码优化

--- src/routes/errors.rs
+++ src/routes/errors.rs
@@ -4,54 +4,63 @@
 
 use std::collections::HashMap;
 
+use once_cell::sync::Lazy;
 use rocket::http::Status;
 use rocket::Request;
 use serde_json::Value::Null;
 
 use crate::routes::APIResponse;
 
+// 业务错误码
+const ERROR_CODE: Lazy<HashMap<u32, &'static str>> = Lazy::new(|| {
+    let mut code = HashMap::new();
+    // Token:
+    code.insert(10001, "Invalid Token");
+    code.insert(10002, " Expired Signature");
+    code
+});
+
 #[catch(400)]
 pub fn bad_request_handler() -> APIResponse {
-    APIResponse::new(400, "Bad Request", Null)
+    APIResponse::new(Status::BadRequest.code.into(), "Bad Request", Null)
 }
 
 #[catch(401)]
 pub fn unauthorized_handler() -> APIResponse {
-    APIResponse::new(401, "Unauthorized", Null)
+    APIResponse::new(Status::Unauthorized.code.into(), "Unauthorized", Null)
 }
 
 #[catch(403)]
 pub fn forbidden_handler() -> APIResponse {
-    APIResponse::new(403, "Forbidden", Null)
+    APIResponse::new(Status::Forbidden.code.into(), "Forbidden", Null)
 }
 
 #[catch(404)]
 pub fn not_found_handler() -> APIResponse {
-    APIResponse::new(404, "Not Found", Null)
+    APIResponse::new(Status::NotFound.code.into(), "Not Found", Null)
 }
 
 #[catch(500)]
 pub fn internal_server_error_handler() -> APIResponse {
-    APIResponse::new(500, "Internal Server Error", Null)
+    APIResponse::new(
+        Status::InternalServerError.code.into(),
+        "Internal Server Error",
+        Null,
+    )
 }
 
 #[catch(503)]
 pub fn service_unavailable_handler() -> APIResponse {
-    APIResponse::new(503, "Service Unavailable", Null)
-}
-
-fn buinss_status_code() -> HashMap<u32, &'static str> {
-    let mut code = HashMap::new();
-    code.insert(10001, "Token: Invalid Token");
-    code.insert(10002, "Token: Expired Signature");
-    code
+    APIResponse::new(
+        Status::ServiceUnavailable.code.into(),
+        "Service Unavailable",
+        Null,
+    )
 }
 
 #[catch(default)]
 pub fn default_catcher(status: Status, _req: &Request) -> APIResponse {
-    let status_code = buinss_status_code();
-    let v = status_code.get(&(status.code as u32));
-    match v {
+    match ERROR_CODE.get(&(status.code as u32)) {
         Some(msg) => APIResponse::new(status.code as u32, msg, Null),
         None => APIResponse::new(status.code as u32, "unknown error", Null),
     }
