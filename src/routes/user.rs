b61a50eb98eb4c626f9e85b08a5ce01c678a8108
silent-rain <silent_rains@1163.com>
2022-08-06 21:58:04 +0800

    接口返回值优化

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -3,7 +3,7 @@
  */
 use log;
 use rocket::serde::json::Json;
-use serde_json::{json, Value};
+use serde_json::json;
 
 use crate::config;
 use crate::database::user::UserData;
@@ -14,29 +14,21 @@ use crate::routes::APIResponse;
 
 // 注册用户
 #[post("/user/register", format = "application/json", data = "<user>")]
-pub async fn register_user(db: DbConn, user: Json<RegisterUser>) -> Json<Value> {
+pub async fn register_user(db: DbConn, user: Json<RegisterUser>) -> APIResponse {
     let result = db
         .run(move |conn| RegisterUser::register_user(user.into_inner(), conn))
         .await;
 
     if let Err(err) = result {
         log::error!("注册用户失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "注册用户失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("注册用户失败");
     }
-    Json(json!({
-        "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 用户登录
 #[post("/user/login", format = "application/json", data = "<user>")]
-pub async fn login(db: DbConn, user: Json<Login>) -> Json<Value> {
+pub async fn login(db: DbConn, user: Json<Login>) -> APIResponse {
     let result = db
         .run(|conn| Login::login(user.into_inner(), conn).map_err(|e| e.to_string()))
         .await;
@@ -44,24 +36,12 @@ pub async fn login(db: DbConn, user: Json<Login>) -> Json<Value> {
     if let Err(err) = result {
         if &err.to_string() == "NotFound" {
             log::error!("用户或密码错误, err: {:#?}", err);
-            return Json(json!({
-                "code": 500,
-                "msg": "用户或密码错误",
-                "data": Value::Null,
-            }));
+            return APIResponse::build().code(0).msg("用户或密码错误");
         }
         log::error!("用户登录失败, err: {:#?}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "用户登录失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("用户登录失败");
     }
-    Json(json!({
-        "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 获取用户列表
@@ -89,65 +69,40 @@ pub async fn get_user_info(auth: Auth, db: DbConn) -> APIResponse {
 
 // 获取用户列表
 #[get("/user/all")]
-pub async fn get_all(db: DbConn) -> Json<Value> {
+pub async fn get_all(db: DbConn) -> APIResponse {
     let result = db.run(move |conn| User::get_all_users(conn)).await;
 
     if let Err(err) = result {
         log::error!("获取用户列表失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "获取用户列表失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("获取用户列表失败");
     }
-    Json(json!({
-        "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 删除用户
 #[get("/delete/<user>")]
-pub async fn delete_user(db: DbConn, user: String) -> Json<Value> {
+pub async fn delete_user(db: DbConn, user: String) -> APIResponse {
     let result = db.run(move |conn| User::delete_by_name(user, conn)).await;
 
     if let Err(err) = result {
         log::error!("删除用户失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "删除用户失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("删除用户失败");
     }
-    Json(json!({
-        "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 根据user更新phone
 #[get("/update_name/<user>/<phone>")]
-pub async fn update_first_name(db: DbConn, user: String, phone: String) -> Json<Value> {
+pub async fn update_first_name(db: DbConn, user: String, phone: String) -> APIResponse {
     let result = db
         .run(move |conn| User::update_by_username(user, phone, conn))
         .await;
 
     if let Err(err) = result {
         log::error!("更新用户手机号码失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "更新用户手机号码失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("更新用户手机号码失败");
     }
-
-    Json(json!({
-        "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 根据id更新指定字段
@@ -156,47 +111,30 @@ pub async fn update_first_name(db: DbConn, user: String, phone: String) -> Json<
     format = "application/json",
     data = "<update_user>"
 )]
-pub async fn updateall(db: DbConn, update_user: Json<User>) -> Json<Value> {
+pub async fn updateall(db: DbConn, update_user: Json<User>) -> APIResponse {
     let result = db
         .run(move |conn| User::update_all(update_user.into_inner(), conn))
         .await;
 
     if let Err(err) = result {
         log::error!("更新用户信息失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "更新用户信息失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("更新用户信息失败");
     }
-
-    Json(json!({
-       "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 // 根据user获取数据
 #[post("/get_user", format = "application/json", data = "<user_data>")]
-pub async fn find_user(db: DbConn, user_data: Json<UserData>) -> Json<Value> {
+pub async fn find_user(db: DbConn, user_data: Json<UserData>) -> APIResponse {
     let result = db
         .run(move |conn| User::get_user_by_username(user_data.into_inner(), conn))
         .await;
 
     if let Err(err) = result {
         log::error!("查询用户信息失败, err: {}", err);
-        return Json(json!({
-            "code": 500,
-            "msg": "查询用户信息失败",
-            "data": Value::Null,
-        }));
+        return APIResponse::build().code(0).msg("查询用户信息失败");
     }
-    Json(json!({
-       "code": 200,
-        "msg": "",
-        "data": result.unwrap(),
-    }))
+    return APIResponse::build().code(200).data(json!(result.unwrap()));
 }
 
 #[cfg(test)]
